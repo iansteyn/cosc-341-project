f1d69b6f44ed815ffa4eeda19845c9d758f52c01
iansteyn <iansteyn03@gmail.com>
2024-11-27 14:52:19 -0800

    add getters to Post

--- app/src/main/java/com/example/cosc341_project/data_classes/Post.java
+++ app/src/main/java/com/example/cosc341_project/data_classes/Post.java
@@ -12,4 +12,43 @@ public class Post {
     private int numDislikes;
     private String[] tags;
     private Comment[] comments;
+
+    // GETTERS
+    public int getPostId() {
+        return postId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public int getNumLikes() {
+        return numLikes;
+    }
+
+    public int getNumDislikes() {
+        return numDislikes;
+    }
+
+    public String[] getTags() {
+        return tags;
+    }
+
+    public Comment[] getComments() {
+        return comments;
+    }
+
+
 }
