9da48122c5570ef1159e7fa358481340d1bd98b3
iansteyn <iansteyn03@gmail.com>
2024-11-26 16:47:09 -0800

    Rename 'notifications' stuff to 'post' stuff

--- app/src/main/java/com/example/cosc341_project/ui/post/PostViewModel.java
+++ app/src/main/java/com/example/cosc341_project/ui/post/PostViewModel.java
@@ -1,16 +1,16 @@
-package com.example.cosc341_project.ui.notifications;
+package com.example.cosc341_project.ui.post;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class PostViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public PostViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is post fragment");
     }
 
     public LiveData<String> getText() {
