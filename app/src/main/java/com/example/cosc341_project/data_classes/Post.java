231db337ed5ad187e5aa7cfe1410834ac4c0bded
iansteyn <iansteyn03@gmail.com>
2024-11-27 16:59:49 -0800

    Add setters for tags and comments + add JavaDoc

--- app/src/main/java/com/example/cosc341_project/data_classes/Post.java
+++ app/src/main/java/com/example/cosc341_project/data_classes/Post.java
@@ -1,22 +1,42 @@
 package com.example.cosc341_project.data_classes;
 
 import java.sql.Timestamp;
+import java.util.Iterator;
 import java.util.LinkedList;
 
-
+/**
+ * <h4> Notes </h4>
+ * <p>
+ *     This is a data class for storing information related to posts. It is a supertype of <code>sightingPost</code>
+ * </p>
+ * <ul>
+ *     <li> All attributes are private. </li>
+ *     <li> All attributes have getters. </li>
+ *     <li>
+ *         Not all attributes have setters.
+ *         <ul>
+ *             <li> <code>PostId</code>, <code>UserId</code>, and <code>timestamp</code> are final (immutable) and initialized at construction. </li>
+ *             <li> <code>numDislikes</code>, <code>numLikes</code> and <code>comments</code> are initialized as 0, 0 and empty list. These all have <b><i>add</i></b> and <b><i>remove</i></b> methods. </li>
+ *             <li> There are no default initialized values for <code>title</code>, <code>description</code> and <code>tags</code>. The user is forced to use their <b><i>setters</i></b> to set these even if they are supposed to be empty. </li>
+ *
+ *         </ul>
+ *     </li>
+ * </ul>
+ *
+ */
 public class Post {
     // ATTRIBUTES
-    private int postId;
+    private int postId; // will probably also be final later on
     private final int userId;
     private final Timestamp timestamp;
 
     private String title;
     private String description;
+    private LinkedList<String> tags;
+
     private int numLikes;
     private int numDislikes;
-
-    public LinkedList<String> tags;
-    public LinkedList<Comment> comments;
+    private LinkedList<Comment> comments;
 
     // CONSTRUCTORS
     public Post(int userId) {
@@ -25,8 +45,6 @@ public class Post {
 
         numLikes = 0;
         numDislikes = 0;
-
-        tags = new LinkedList<String>();
         comments = new LinkedList<Comment>();
     }
 
@@ -49,25 +67,35 @@ public class Post {
 
     public void setTitle(String title) { this.title = title; }
     public void setDescription(String description) { this.description = description; }
+    public void setTags(LinkedList<String> tags) { this.tags = tags; }
 
     public void addLike() { numLikes++; }
     public void removeLike() { numLikes--; }
     public void addDislike() { numDislikes++; }
     public void removeDislike() { numDislikes--; }
 
-    //TODO - should I have setters and getters for tags and comments?
+    public boolean addComment(Comment newComment) {
+        // Returns true if successful, false otherwise
+        return comments.add(newComment);
+    }
+    public boolean removeComment(int commentId) {
+        // Returns true if successful, false otherwise
+
+        Iterator<Comment> commentsIterator = comments.iterator();
+
+        while (commentsIterator.hasNext()) {
+            if (commentsIterator.next().commentId == commentId) {
+                commentsIterator.remove();
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
 
 // THOUGHTS
-/* If the way that post editing works is you overwrite a whole post object,
- * then the only setters we need are for likes, dislikes, tag and comments.
- * Everything else can go in the constructor.
- *
- * NO wait that doesn't make sense. The postId will be passed to the 'edit'
- * page when that button gets pressed on the feed. Then the file will have to
- * read the post info, pre-populate the fields with all that info...
- *
- * then we can choose to either save the post as a completely new post (which
- * seems wrong) well... actually I just make it so that it saves as a 'new' post
- * object, but then in the file it checks if postId already exists...
+/*
+ * The "editing mode" of Evan's stuff will get the whole post object passed to it.
+ * Modifications are made, and then that post object is saved using 'saveEditedPost'
  */
