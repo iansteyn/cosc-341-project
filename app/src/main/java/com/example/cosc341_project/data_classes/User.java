d2608f831a79222b49751fbfc4e1cadd7d1b079d
iansteyn <iansteyn03@gmail.com>
2024-11-30 13:13:06 -0800

    Add User class
    with attributes, constructor and getters

---
+++ app/src/main/java/com/example/cosc341_project/data_classes/User.java
@@ -0,0 +1,26 @@
+package com.example.cosc341_project.data_classes;
+
+public class User {
+    // ATTRIBUTES
+    private final int userId;
+    private String userName;
+    private String profilePicName; //not sure about this data type yet, we're still figuring out images
+
+    // CONSTRUCTOR
+    public User(int userId, String userName, String profilePicName) {
+        this.userId = userId;
+        this.userName = userName;
+        this.profilePicName = profilePicName;
+    }
+
+    // GETTERS
+    public int getUserId() {
+        return userId;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public String getProfilePicName() {
+        return profilePicName;
+    }
+}
