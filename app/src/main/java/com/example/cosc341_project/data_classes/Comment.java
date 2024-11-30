67b29eec70c647b509dd5bcc455b4a2f8ed5c89f
iansteyn <iansteyn03@gmail.com>
2024-11-30 12:34:40 -0800

    Add constructor for Comment class

--- app/src/main/java/com/example/cosc341_project/data_classes/Comment.java
+++ app/src/main/java/com/example/cosc341_project/data_classes/Comment.java
@@ -3,8 +3,20 @@ package com.example.cosc341_project.data_classes;
 import java.sql.Timestamp;
 
 public class Comment {
-    public int commentId;
-    public int userId;
-    public String text;
-    Timestamp timestamp;
+    protected int commentId;
+    protected int userId;
+    protected String text;
+    protected Timestamp timestamp;
+
+    /**
+     * This constructor is protected because it is only used in the <code>Post</code> class.
+     * It is not intended for use outside of this package. If you want to add comments
+     * to a post, you have to do so through the <code>addComment</code> method from the <code>Post</code> class.
+     */
+    protected Comment(int commentId, int userId, String text, Timestamp timestamp) {
+        this.commentId = commentId;
+        this.userId = userId;
+        this.text = text;
+        this.timestamp = timestamp;
+    }
 }
