f7ca08ee44f5adc168df55e223af565671c9a802
iansteyn <iansteyn03@gmail.com>
2024-11-30 12:43:36 -0800

    make vars private and add getters

--- app/src/main/java/com/example/cosc341_project/data_classes/Comment.java
+++ app/src/main/java/com/example/cosc341_project/data_classes/Comment.java
@@ -3,20 +3,39 @@ package com.example.cosc341_project.data_classes;
 import java.sql.Timestamp;
 
 public class Comment {
-    protected int commentId;
-    protected int userId;
-    protected String text;
-    protected Timestamp timestamp;
+    // ATTRIBUTES
+    private int commentId;
+    private int userId;
+    private String text;
+    private final Timestamp timestamp;
 
+    // CONSTRUCTORS
     /**
      * This constructor is protected because it is only used in the <code>Post</code> class.
      * It is not intended for use outside of this package. If you want to add comments
      * to a post, you have to do so through the <code>addComment</code> method from the <code>Post</code> class.
      */
-    protected Comment(int commentId, int userId, String text, Timestamp timestamp) {
+    protected Comment(int commentId, int userId, String text) {
         this.commentId = commentId;
         this.userId = userId;
         this.text = text;
-        this.timestamp = timestamp;
+        this.timestamp = new Timestamp(System.currentTimeMillis());
+    }
+
+    // GETTERS
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
     }
 }
