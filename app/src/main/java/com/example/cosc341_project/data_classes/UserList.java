067254bb1d4ac2618a197d799effe1909bfdcc03
iansteyn <iansteyn03@gmail.com>
2024-12-02 10:38:15 -0800

    Create class UserList

---
+++ app/src/main/java/com/example/cosc341_project/data_classes/UserList.java
@@ -0,0 +1,24 @@
+package com.example.cosc341_project.data_classes;
+
+// AHAHAHA LET'S MAKE IT COMPLETELY STATIC
+// #HARDCODEITBABY
+public class UserList {
+    static int CURRENT_USER_ID = 10;
+
+    static User[] users = new User[] {
+        new User(0, "bobby_mohamed", "pfp0.png"),
+        new User(1, "yoMamma", "pfp1.png"),
+        new User(2, "bigfoot_fanatic", "pfp2.png"),
+        new User(3, "OgopoStop", "pfp3.png"),
+        new User(4, "Mothman2000", "pfp4.png"),
+        new User(5, "Jeremy", "pfp5.png"),
+        new User(6, "girl", "pfp6.png"),
+        new User(7, "boy", "pfp7.jpg"),
+        new User(8, "Awkwardfina", "pfp8.jpg"),
+        new User(9, "lakedweller69", "pfp9.jpg"),
+        new User(10, "Bill", "pfp10.png")
+    };
+
+
+
+}
