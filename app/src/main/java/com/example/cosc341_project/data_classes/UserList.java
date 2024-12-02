9b76212c0b3340354f03e9f116bc1197b98f527d
iansteyn <iansteyn03@gmail.com>
2024-12-02 12:40:45 -0800

    Update UserList pfp names

--- app/src/main/java/com/example/cosc341_project/data_classes/UserList.java
+++ app/src/main/java/com/example/cosc341_project/data_classes/UserList.java
@@ -36,17 +36,17 @@ public class UserList {
     public static final int CURRENT_USER_ID = 10;
 
     private static final User[] users = new User[] {
-        new User(0, "bobby_mohamed", "pfp0.png"),
-        new User(1, "yoMamma", "pfp1.png"),
-        new User(2, "bigfoot_fanatic", "pfp2.png"),
-        new User(3, "OgopoStop", "pfp3.png"),
-        new User(4, "Mothman2000", "pfp4.png"),
-        new User(5, "Jeremy", "pfp5.png"),
-        new User(6, "girl", "pfp6.png"),
-        new User(7, "boy", "pfp7.jpg"),
-        new User(8, "Awkwardfina", "pfp8.jpg"),
-        new User(9, "lakedweller69", "pfp9.jpg"),
-        new User(10, "Bill", "pfp10.png")
+        new User(0, "bobby_mohamed", "pfp_mohamed"),
+        new User(1, "yoMamma", "pfp_yomamma"),
+        new User(2, "bigfoot_fanatic", "pfp_bigfoot"),
+        new User(3, "OgopoStop", "pfp_stop"),
+        new User(4, "Mothman2000", "pfp_mothman"),
+        new User(5, "Jeremy", "pfp_jeremy"),
+        new User(6, "girl", "pfp_girl"),
+        new User(7, "boy", "pfp_boy"),
+        new User(8, "Awkwardfina", "pfp_aquafina"),
+        new User(9, "lakedweller69", "pfp_lakewoman"),
+        new User(10, "Bill", "pfp_bill")
     };
 
     // STATIC METHODS
