2214499232a4a945a85c319ae3aec03f41248cbd
iansteyn <iansteyn03@gmail.com>
2024-12-04 12:34:06 -0800

    Remove plm code from FeedViewModel

--- app/src/main/java/com/example/cosc341_project/ui/feed/FeedViewModel.java
+++ app/src/main/java/com/example/cosc341_project/ui/feed/FeedViewModel.java
@@ -11,21 +11,10 @@ import com.example.cosc341_project.data_classes.*;
 public class FeedViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
-    private final PostListManager plm;
 
     public FeedViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is feed fragment");
-
-        plm = PostListManager.getInstance();
-
-        plm.addFakePosts();
-        String [] sightingPostTags = {"sasquatch", "sighting"};
-
-        Post tempPost = plm.postList.get(0);
-        tempPost.addComment(123, "This is an example comment");
-        plm.postList.add(new SightingPost(123, "Sighting post", "Example description", sightingPostTags, "nullimage", "Kelowna, BC"));
-
     }
 
     public LiveData<String> getText() {
