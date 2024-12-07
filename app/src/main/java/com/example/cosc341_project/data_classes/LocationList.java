9e021086673d85f449c95f726da61c5bdd7b7b92
iansteyn <iansteyn03@gmail.com>
2024-12-06 21:44:09 -0800

    Add more locations

--- app/src/main/java/com/example/cosc341_project/data_classes/LocationList.java
+++ app/src/main/java/com/example/cosc341_project/data_classes/LocationList.java
@@ -7,9 +7,11 @@ public class LocationList {
     private static HashMap<String, double[]> locationList;
     static {
         locationList = new HashMap<>();
-        locationList.put("Mission Creek", new double[]{49.8625, -119.4550});
-        locationList.put("Bear Creek Provincial Park", new double[]{49.9152, -119.5126});
         locationList.put("UBCO", new double[]{49.9394, -119.3948});
+        locationList.put("Glenrosa", new double[]{49.8337, -119.6605});
+        locationList.put("Kin Beach, Vernon", new double[]{50.2493, -119.3486});
+        locationList.put("Penticton City Center", new double[]{49.4991, -119.5937});
+        locationList.put("Rutland", new double[]{49.8900, -119.3954});
         locationList.put("Select Location", null);
     }
 
